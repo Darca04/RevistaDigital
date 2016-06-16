acd338560552ddcb133fdd68a88df329ca80dc6e
Darren Campos <darrencam_04@hotmail.com>
2016-06-15 23:36:46 -0600

    cambio de letras

---
+++ RevistaDigital/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RevistaDigital.Startup))]
+namespace RevistaDigital
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
